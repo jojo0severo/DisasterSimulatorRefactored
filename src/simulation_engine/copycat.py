d30e2d5a87536598d31fc29e9ecb0e9a6f17c517
jojo <joaovbsevero@gmail.com>
2019-06-26 18:32:35 -0300

    Implemented logs in the copycat

--- src/simulation_engine/copycat.py
+++ src/simulation_engine/copycat.py
@@ -4,6 +4,8 @@ from src.simulation_engine.simulation import Simulation
 
 class CopyCat:
     def __init__(self, config):
+        self.config = config
+        self.logs = {}
         self.simulation = Simulation(config)
 
     def connect_agent(self, token):
@@ -21,3 +23,14 @@ class CopyCat:
     def do_step(self, agent_action_list):
         response = self.simulation.do_step(agent_action_list)
         return copy.deepcopy(response)
+
+    def get_logs(self):
+        return copy.deepcopy(
+            [
+                datetime.datetime.now().year,
+                datetime.datetime.now().strftime('%B'),
+                datetime.datetime.now().strftime('%A'),
+                datetime.datetime.now().hour,
+                datetime.datetime.now().minute,
+                self.logs
+            ])
