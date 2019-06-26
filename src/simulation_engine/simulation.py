932eb00f5468c43ee6d80c0c84be4a10ee5f65e9
jojo <joaovbsevero@gmail.com>
2019-06-26 18:33:21 -0300

    Implemented logs in the simulation

--- src/simulation_engine/simulation.py
+++ src/simulation_engine/simulation.py
@@ -5,6 +5,10 @@ class Simulation:
     def __init__(self, config):
         self.cycler = Cycle(config)
         self.terminated = False
+        self.actions_amount = 0
+        self.actions_amount_by_step = []
+        self.actions_by_step = []
+        self.action_token_by_step = []
 
     def connect_agent(self, token):
         return self.cycler.connect_agent(token)
@@ -31,6 +35,15 @@ class Simulation:
         if self.terminated:
             return None
 
+
+        actions = [token_action_param['action'] for token_action_param in agent_action_list]
+        tokens = [token_action_param['token'] for token_action_param in agent_action_list]
+
+        self.actions_amount += len(agent_action_list)
+        self.actions_amount_by_step.append((self.cycler.current_step, len(agent_action_list)))
+        self.actions_by_step.append((self.cycler.current_step, actions))
+        self.action_token_by_step.append((self.cycler.current_step, zip(tokens, actions)))
+
         actions_results = self.cycler.execute_actions(agent_action_list)
         step = self.cycler.get_step()
 
@@ -44,3 +57,99 @@ class Simulation:
         self.cycler.activate_step()
 
         return actions_results, step
+
+    def log(self):
+        current_step = self.cycler.current_step
+        max_steps = self.cycler.max_steps
+        delivered_items = self.cycler.delivered_items
+        agents_amount = len(self.cycler.agents_manager.get_tokens())
+        agents = self.cycler.agents_manager.get_agents_info()
+        active_agents_amount = sum([1 if agent.active else 0 for agent in self.cycler.agents_manager.get_agents_info()])
+        active_agents = [agent for agent in self.cycler.agents_manager.get_agents_info() if agent.active]
+
+        floods_amount = 0
+        victims_saved = 0
+        victims_dead = 0
+        photos_taken = 0
+        photos_ignored = 0
+        water_samples_collected = 0
+        water_samples_ignored = 0
+
+        for i in range(current_step):
+            if not self.cycler.steps[i]['flood']:
+                continue
+
+            floods_amount += 1
+
+            for victim in self.cycler.steps[i]['victims']:
+                if not victim.active and victim.lifetime:
+                    victims_saved += 1
+
+                elif not victim.active:
+                    victims_dead += 1
+
+            for photo in self.cycler.steps[i]['photos']:
+                if not photo.active:
+                    photos_taken += 1
+
+                    for victim in photo.victims:
+                        if not victim.active and victim.lifetime:
+                            victims_saved += 1
+
+                        elif not victim.active:
+                            victims_dead += 1
+
+                else:
+                    photos_ignored += 1
+
+            for water_sample in self.cycler.steps[i]['water_samples']:
+                if not water_sample.active:
+                    water_samples_collected += 1
+
+                else:
+                    water_samples_ignored += 1
+
+        return {
+            'environment': {
+                'current_step': current_step,
+                'max_steps': max_steps,
+                'delivered_items': delivered_items,
+                'floods_amount': floods_amount,
+                'total_victims': self.cycler.max_victims,
+                'victims_saved': victims_saved,
+                'victims_dead': victims_dead,
+                'total_photos': self.cycler.max_photos,
+                'photos_taken': photos_taken,
+                'photos_ignored': photos_ignored,
+                'total_water_samples': self.cycler.max_water_samples,
+                'water_samples_collected': water_samples_collected,
+                'water_samples_ignored': water_samples_ignored,
+                'total_social_assets': self.cycler.max_social_assets
+            },
+            'agents': {
+                'active_agents_amount': active_agents_amount,
+                'agents_amount': agents_amount,
+                'active_agents': active_agents,
+                'agents': agents,
+            },
+            'actions': {
+                'amount_of_actions_executed': self.actions_amount,
+                'amount_of_actions_by_step': self.actions_amount_by_step,
+                'actions_by_step': self.actions_by_step,
+                'action_token_by_step': self.action_token_by_step
+            },
+        }
+
+    def __del__(self):
+        del self.cycler
+        del self.terminated
+        del self.actions_amount
+
+        self.actions_amount_by_step.clear()
+        del self.actions_amount_by_step
+
+        self.actions_by_step.clear()
+        del self.actions_by_step
+
+        self.action_token_by_step.clear()
+        del self.action_token_by_step
