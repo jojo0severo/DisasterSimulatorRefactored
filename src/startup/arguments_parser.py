1a0309b0d8cf0093d349a57a1ac18c9033a110b5
jojo <joaovbsevero@gmail.com>
2019-07-01 18:44:06 -0300

    Removing classes from one file
    All classes responsible for the startup are now in the startup directory inside source

---
+++ src/startup/arguments_parser.py
@@ -0,0 +1,42 @@
+import argparse
+
+
+class Parser:
+    def __init__(self):
+        self.parser = argparse.ArgumentParser(prog='Disaster Simulator')
+        self.add_arguments()
+
+    def add_arguments(self):
+        self.parser.add_argument('-conf', required=True, type=str)
+        self.parser.add_argument('-url', required=False, type=str, default='127.0.0.1')
+        self.parser.add_argument('-sp', required=False, type=str, default='8910')
+        self.parser.add_argument('-ap', required=False, type=str, default='12345')
+        self.parser.add_argument('-pyv', required=False, type=str, default='')
+        self.parser.add_argument('-g', required=False, type=bool, default=False)
+        self.parser.add_argument('-step_t', required=False, type=int, default=30)
+        self.parser.add_argument('-first_t', required=False, type=int, default=60)
+        self.parser.add_argument('-mtd', required=False, type=str, default='time')
+
+    def get_argument(self, arg):
+        args = self.parser.parse_args()
+        for argument in args.__dict__:
+            if argument == arg:
+                return args.__dict__[arg]
+
+        return None
+
+    def get_simulation_arguments(self):
+        args = self.parser.parse_args()
+
+        return [args.conf, args.url, args.sp, args.ap]
+
+    def get_api_arguments(self):
+        args = self.parser.parse_args()
+
+        return [args.url, args.ap, args.sp, args.step_t, args.first_t, args.mtd]
+
+    def get_arguments(self):
+        args = self.parser.parse_args()
+
+        return [args.conf, args.url, args.sp, args.ap, args.pyv, args.g,
+                args.step_t, args.first_t, args.mtd]
---
+++ src/startup/thread_starter.py
@@ -0,0 +1,83 @@
+import json
+import secrets
+import pathlib
+import subprocess
+from multiprocessing import Process
+from src.startup.environment_handler import Handler
+from src.startup.arguments_parser import Parser
+from src.startup.configuration_checker import Checker
+
+
+class Starter:
+    def __init__(self):
+        self.parser = Parser()
+        self.env_handler = Handler()
+        self.checker = Checker(self.parser.get_argument('conf'))
+        self.root = pathlib.Path(__file__).parents[1].absolute()
+
+    def start(self):
+        self.check_configuration_file()
+        self.create_environment()
+        arguments = self.get_arguments()
+        self.start_processes(*arguments)
+
+    def check_configuration_file(self):
+        test = self.checker.test_json_load()
+        if test[0]:
+            test = self.checker.test_main_keys()
+            if test[0]:
+                test = self.checker.test_map_key()
+                if test[0]:
+                    test = self.checker.test_agents_key()
+                    if test[0]:
+                        test = self.checker.test_roles_key()
+                        if test[0]:
+                            test = self.checker.test_generate_key()
+                            if test[0]:
+                                return
+        exit(test[1])
+
+    def create_environment(self):
+        globally = self.parser.get_argument('g')
+        python_version = self.parser.get_argument('pyv')
+
+        self.env_handler.create_environment(globally, python_version)
+
+    def get_arguments(self):
+        config_file = self.parser.get_argument('conf')
+
+        with open(config_file, 'r') as configuration_file:
+            agents_amount = sum(json.load(configuration_file)['agents'].values())
+
+        secret = secrets.token_urlsafe(15)
+
+        simulation_arguments = self.parser.get_simulation_arguments()
+        simulation_arguments.append(secret)
+
+        api_arguments = self.parser.get_api_arguments()
+        api_arguments.extend([agents_amount, secret])
+
+        return api_arguments, simulation_arguments, self.parser.get_argument('pyv')
+
+    def start_processes(self, api_arguments, simulation_arguments, python_version):
+        simulation_path = str((self.root / 'execution' / 'simulation.py').absolute())
+        simulation_process_arguments = (simulation_path, simulation_arguments, self.env_handler.venv_path, python_version)
+        simulation_process = Process(target=self.start_simulation, args=simulation_process_arguments, daemon=True)
+
+        api_path = str((self.root / 'execution' / 'api.py').absolute())
+        api_process_arguments = (api_path, api_arguments, self.env_handler.venv_path, python_version)
+        api_process = Process(target=self.start_api, args=api_process_arguments, daemon=True)
+
+        api_process.start()
+        simulation_process.start()
+
+        api_process.join()
+        simulation_process.join()
+
+    @staticmethod
+    def start_simulation(module_path, simulation_arguments, venv_path, python_version):
+        subprocess.call([f'{str(venv_path)}python{python_version}', module_path, *map(str, simulation_arguments)])
+
+    @staticmethod
+    def start_api(module_path, api_arguments, venv_path, python_version):
+        subprocess.call([f"{str(venv_path)}python{python_version}", module_path, *map(str, api_arguments)])
