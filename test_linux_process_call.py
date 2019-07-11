b30e29db0a51bf6e19e8c4e6d1343546959855f2
jojo0severo <joaovbsevero@gmail.com>
2019-07-11 14:11:32 -0300

    Fixed

--- test_linux_process_call.py
+++ test_linux_process_call.py
@@ -1,7 +1,14 @@
+import os
 import subprocess
 import pathlib
 
 
 if __name__ == '__main__':
-    python_path = pathlib.Path(__file__).parent / 'venv' / 'bin' / 'python'
-    subprocess.call(str(python_path.absolute()))
+    if os.name == 'nt':
+        python_path = pathlib.Path(__file__).parent / 'venv' / 'Scripts' / 'python.exe'
+    else:
+        python_path = pathlib.Path(__file__).parent / 'venv' / 'bin' / 'python'
+
+    file_path = pathlib.Path(__file__).parent / 'test_linux_process_called.py'
+
+    subprocess.call([str(python_path.absolute()), str(file_path.absolute())])
