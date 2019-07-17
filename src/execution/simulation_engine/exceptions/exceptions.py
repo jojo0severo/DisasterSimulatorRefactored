99eb3d8bd308297d4203c5880ad48ad4ff1942b8
jojo0severo <joaovbsevero@gmail.com>
2019-07-17 17:15:29 -0300

    Added string value for exceptions
    To enable the possibility of using the str() functions from python on the exceptions instead of using the message attribute everytime

--- src/execution/simulation_engine/exceptions/exceptions.py
+++ src/execution/simulation_engine/exceptions/exceptions.py
@@ -4,11 +4,17 @@ class FailedWrongParam(Exception):
     def __init__(self, message=None):
         self.message = message
 
+    def __str__(self):
+        return 'FailedWrongParam: ' + self.message
+
 
 class FailedUnknownFacility(Exception):
 
     def __init__(self, message=None):
         self.message = message
+
+    def __str__(self):
+        return 'FailedUnknownFacility: ' + self.message
 
 
 class FailedNoRoute(Exception):
@@ -16,11 +22,17 @@ class FailedNoRoute(Exception):
     def __init__(self, message=None):
         self.message = message
 
+    def __str__(self):
+        return 'FailedNoRoute: ' + self.message
+
 
 class FailedCapacity(Exception):
 
     def __init__(self, message):
         self.message = message
+
+    def __str__(self):
+        return 'FailedCapacity: ' + self.message
 
 
 class FailedLocation(Exception):
@@ -28,11 +40,17 @@ class FailedLocation(Exception):
     def __init__(self, message):
         self.message = message
 
+    def __str__(self):
+        return 'FailedLocation: ' + self.message
+
 
 class FailedUnknownItem(Exception):
 
     def __init__(self, message):
         self.message = message
+
+    def __str__(self):
+        return 'FailedUnknownItem: ' + self.message
 
 
 class FailedItemAmount(Exception):
@@ -40,11 +58,17 @@ class FailedItemAmount(Exception):
     def __init__(self, message):
         self.message = message
 
+    def __str__(self):
+        return 'FailedItemAmount: ' + self.message
+
 
 class FailedInvalidKind(Exception):
 
     def __init__(self, message):
         self.message = message
+
+    def __str__(self):
+        return 'FailedInvalidKind: ' + self.message
 
 
 class FailedInsufficientBattery(Exception):
@@ -52,14 +76,23 @@ class FailedInsufficientBattery(Exception):
     def __init__(self, message):
         self.message = message
 
+    def __str__(self):
+        return 'FailedInsufficientBattery: ' + self.message
+
 
 class FailedNoSocialAsset(Exception):
 
     def __init__(self, message):
         self.message = message
 
+    def __str__(self):
+        return 'FailedNoSocialAsset: ' + self.message
+
 
 class UnableToReach(Exception):
 
     def __init__(self, message):
         self.message = message
+
+    def __str__(self):
+        return 'UnableToReach: ' + self.message
